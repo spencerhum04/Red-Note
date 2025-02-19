099248cbaf9aaa4cb7e08332d408b0ebc0aa5a00
Spencer Hum <spencerhum@Spencers-MacBook-Air.local>
2025-02-18 20:44:25 -0500

    feat: a1 video

--- src/routes/landing/sections/assignment-one.tsx
+++ src/routes/landing/sections/assignment-one.tsx
@@ -8,6 +8,7 @@ export default function AssignmentOne() {
                 <div className="button-text mb-2"></div>
                 <a href="/CISC322_Assignment_1.pdf" target="_blank" rel="noopener noreferrer" className="button-style bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">A1 Report</a>
                 <a href="/CISC322_A1_Presentation.pdf" target="_blank" rel="noopener noreferrer" className=" ml-4 button-style bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">A1 Presentation</a>
+                <a href="https://www.youtube.com/watch?v=6bVpLzd3rXA&ab_channel=Polarisa" target="_blank" rel="noopener noreferrer" className=" ml-4 button-style bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">A1 Video</a>
             </div>
             
         </div>
