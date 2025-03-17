1dacf7cef05cfb400fda8596438d787df6751793
Spencer Hum <spencerhum@Spencers-MacBook-Air.local>
2025-03-17 11:21:34 -0400

    feat: A2

--- src/routes/landing/sections/assignment-two.tsx
+++ src/routes/landing/sections/assignment-two.tsx
@@ -2,20 +2,15 @@ import { Link } from "react-router-dom";
 export default function AssignmentTwo() {
     return (
         <div className="w-full flex flex-col text-black px-6 h-40">
-            <div className="text-xl font-semibold">Assignment 2</div>
+            <div className="text-xl font-semibold">Assignment 1</div>
             <div>This is where the assignment contents will go.</div>
 
-            {/* add a button that jump to another page for further on information */}
-            <div className="button-container mt-4">
+            <div className="mt-4">
                 <div className="button-text mb-2"></div>
-                <Link
-                    to="A2"
-                    className="button-style bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-                >
-                    A2 Detail
-                </Link>
+                <a href="/CISC322_Assignment_2.pdf" target="_blank" rel="noopener noreferrer" className="button-style bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">A2 Report</a>
+                <a href="/CISC322_A2_Presentation.pdf" target="_blank" rel="noopener noreferrer" className=" ml-4 button-style bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">A2 Presentation</a>
+                <a href="https://www.youtube.com/watch?v=ps322cRxQbc&ab_channel=Polarisa" target="_blank" rel="noopener noreferrer" className=" ml-4 button-style bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">A2 Video</a>
             </div>
-
         </div>
     )
 }
