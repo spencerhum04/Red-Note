7578ce1e0b32ac0e7c5ce13554b080fd8b62097f
Spencer Hum <spencerhum@Spencers-MacBook-Air.local>
2025-03-17 11:26:07 -0400

    fix: a2 title

--- src/routes/landing/sections/assignment-two.tsx
+++ src/routes/landing/sections/assignment-two.tsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom";
 export default function AssignmentTwo() {
     return (
         <div className="w-full flex flex-col text-black px-6 h-40">
-            <div className="text-xl font-semibold">Assignment 1</div>
+            <div className="text-xl font-semibold">Assignment 2</div>
             <div>This is where the assignment contents will go.</div>
 
             <div className="mt-4">
